0bb4dbc1c2ae1a655bd8f4ce6b8437ceae7d2ecb
Davies Esiro <ojurereoluwad@gmail.com>
2020-08-31 00:45:30 +0100

    🕵🏿‍♀️ validation and error handling ✅

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -31,7 +31,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -49,6 +55,9 @@ export class TasksService {
 
   deleteTask(id: string): Task {
     const taskIndex = this.tasks.findIndex(task => task.id === id);
+    if (!taskIndex) {
+      throw new NotFoundException(`Task with id '${id}' not found`);
+    }
     return this.tasks.splice(taskIndex, 1)[0];
   }
 
